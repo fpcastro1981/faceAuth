6c8e6b0ff6e1573a1e83d016afbc9a7336b065b1
Fernando Pereira de Castro <c1281432@bb.com.br>
2018-11-23 17:10:34 -0200

    pronto para incluir autenticacao face

---
+++ passport.js
@@ -0,0 +1,49 @@
+const passport = require('passport');
+const JwtStrategy = require('passport-jwt').Strategy;
+const { ExtractJwt } = require('passport-jwt');
+const LocalStrategy = require('passport-local').Strategy;
+
+const { JWT_SECRET } = require('./configuration');
+const User = require('./models/user');
+
+// JSON WEB TOKEN STRATEGY
+passport.use(new JwtStrategy({
+    jwtFromRequest: ExtractJwt.fromHeader('authorization'),
+    secretOrKey: JWT_SECRET
+}, async (payload, done) => {
+    try {
+        // find the user specified in token
+        const user = await User.findById(payload.sub);
+
+        //if doesnt exists handle it
+        if(!user){
+            return done(null, false);
+        }
+
+        //Otherwise, return the user
+        done(null, user);
+
+    }catch (error){
+        done(error, false);
+    }
+}));
+
+
+// LOCAL STRATEGY
+passport.use(new LocalStrategy({
+    usernameField: 'email'
+}, async (email, password, done) => {
+    // find the user given the email
+    const user = await User.findOne({email});
+
+    // if not, handle it
+    if (!user) {
+        return done(null, false);
+    }
+
+    // check if the password is correct
+
+    // if not, handle it
+
+    // otherwise, return the user
+}))
